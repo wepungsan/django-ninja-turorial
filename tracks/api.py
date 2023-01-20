2c79c2d5efa29830683d01c38fb54ab142bfee7b
wepungsan <wepungsan@gmail.com>
2023-01-21 00:05:21 +0900

    video 3

--- tracks/api.py
+++ tracks/api.py
@@ -3,7 +3,9 @@ from ninja import NinjaAPI
 from tracks.models import Track
 from tracks.schema import TrackSchema, NotFoundSchema
 
+
 api = NinjaAPI()
+
 
 @api.get("/tracks", response=List[TrackSchema])
 def tracks(request, title: Optional[str] = None):
@@ -20,6 +22,30 @@ def track(request, track_id: int):
     except Track.DoesNotExist as e:
         return 404, {"message":"Track does not exist"}
 
-# @api.get("/test")
-# def test(request):
-#     return {'test':'success'}
+
+@api.post("/tracks", response={201: TrackSchema})
+def create_track(request, track: TrackSchema):
+    track = Track.objects.create(**track.dict())
+    return track
+
+
+@api.put("/tracks/{track_id}", response={200: TrackSchema, 404: NotFoundSchema})
+def put_track(request, track_id: int, data: TrackSchema):
+    try:
+        track = Track.objects.get(pk=track_id)
+        for attribute, value in data.dict().items():
+            setattr(track, attribute, value)
+        track.save()
+        return 200, track
+    except Track.DoesNotExist as e:
+        return 404, {"message":"Track does not exist"}
+
+
+@api.delete("/tracks/{track_id}", response={200: None, 404: NotFoundSchema})
+def delete_track(request, track_id: int):
+    try:
+        track = Track.objects.get(pk=track_id)
+        track.delete()
+        return 200
+    except Track.DoesNotExist as e:
+        return 404, {"message":"Track does not exist"}
