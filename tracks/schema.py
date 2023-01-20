ba8e213ca31c3b9e481706ba72b788e69e2592c8
wepungsan <wepungsan@gmail.com>
2023-01-21 00:46:36 +0900

    video 5

--- tracks/schema.py
+++ tracks/schema.py
@@ -1,11 +1,21 @@
 from datetime import datetime
-from ninja import Schema
+from ninja import Schema, ModelSchema
+from ninja.orm import create_schema
+from tracks.models import Track
 
-class TrackSchema(Schema):
-    title: str
-    artist: str
-    duration: float
-    last_play: datetime
+
+TrackSchema = create_schema(Track, fields=['title', 'last_play', 'artist', 'duration'])
+
+# class TrackSchema(ModelSchema):
+#     class Config:
+#         model = Track
+#         model_fields = ['title', 'last_play', 'artist', 'duration']
 
 class NotFoundSchema(Schema):
     message: str
+
+# class TrackSchema(Schema):
+#     title: str
+#     artist: str
+#     duration: float
+#     last_play: datetime
